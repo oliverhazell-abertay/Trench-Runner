97de2d018cff5c822f7361a198ac6092db898026
oliverhazell-abertay <1401203@uad.ac.uk>
2021-11-29 21:06:58 +0000

    Bullets face direction in xz plane but not yz yet

--- build/vs2017/bullet.cpp
+++ build/vs2017/bullet.cpp
@@ -1,4 +1,6 @@
 #include "bullet.h"
+#include "maths/math_utils.h"
+#include <cmath>
 
 void Bullet::Update(float deltaTime)
 {
@@ -14,12 +16,16 @@ void Bullet::Update(float deltaTime)
 		SetActive(false);
 	}
 
-	FaceDirectionOfMovement();
+	if (isMoving)
+		FaceDirectionOfMovement();
 
 	GameObject::Update(deltaTime);
 }
 
 void Bullet::FaceDirectionOfMovement()
 {
-
+	float theta = atan(velocity_.x() / velocity_.z());
+	rotation_.set_y(theta);
+	theta = atan(velocity_.y() / velocity_.z());
+	rotation_.set_x(theta);
 }
